aab867fd55efac6ff7fb7eeda4add687c7df1d21
Tom Genoni <tom@genoni.studio>
2026-01-19 14:36:59 -0800

    Add build for now

---
+++ build/ios/dark/StyleDictionarySize.h
@@ -0,0 +1,31 @@
+
+// StyleDictionarySize.h
+//
+
+// Do not edit directly, this file was auto-generated.
+
+
+#import <Foundation/Foundation.h>
+
+
+extern const dimensionScale;
+extern const dimensionXs;
+extern const dimensionSm;
+extern const dimensionMd;
+extern const dimensionLg;
+extern const dimensionXl;
+extern const spacingXs;
+extern const spacingSm;
+extern const spacingMd;
+extern const spacingLg;
+extern const spacingXl;
+extern const spacingMultiValue;
+extern const borderRadiusSm;
+extern const borderRadiusLg;
+extern const borderRadiusXl;
+extern const borderRadiusMultiValue;
+extern const letterSpacingDefault;
+extern const letterSpacingIncreased;
+extern const letterSpacingDecreased;
+extern const paragraphSpacingH1;
+extern const paragraphSpacingH2;
---
+++ build/ios/light/StyleDictionarySize.m
@@ -0,0 +1,32 @@
+
+//
+// StyleDictionarySize.m
+//
+
+// Do not edit directly, this file was auto-generated.
+
+
+#import ".h"
+
+
+const dimensionScale = 4px;
+const dimensionXs = 4px;
+const dimensionSm = 16px;
+const dimensionMd = 64px;
+const dimensionLg = 256px;
+const dimensionXl = 1024px;
+const spacingXs = 4px;
+const spacingSm = 16px;
+const spacingMd = 64px;
+const spacingLg = 256px;
+const spacingXl = 1024px;
+const spacingMultiValue = 16px 1024px;
+const borderRadiusSm = 4px;
+const borderRadiusLg = 8px;
+const borderRadiusXl = 16px;
+const borderRadiusMultiValue = 4px 8px;
+const letterSpacingDefault = 0;
+const letterSpacingIncreased = 1.5em;
+const letterSpacingDecreased = -0.05em;
+const paragraphSpacingH1 = 32px;
+const paragraphSpacingH2 = 26px;
